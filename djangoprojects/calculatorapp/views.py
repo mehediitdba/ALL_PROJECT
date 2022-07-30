26f63d3b83e44579abc6792241e4344d2ab30968
mehediitdba <mehediitdba@gmail.com>
2022-07-30 12:33:06 +0600

    added

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 def index(request):
     #return HttpResponse("calculater app server started")
     return render(request, 'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    #return HttpResponse(q)
+    try:
+        ans = eval(q)
+        mydictionary={
+            "q":q,
+            "ans":ans,
+            "error":False,
+            "result":True
+        }
+        return render(request, 'index.html', context=mydictionary)
+    except:
+        mydictionary={
+            "error": True,
+            "result":False
+        }
+        return render(request, 'index.html', context=mydictionary)
+
+    #jsonedic={
+    #    "q":q
+    #}
+    #return JsonResponse(jsonedic)
--- djangoprojects/rootapp/views.py
+++ djangoprojects/rootapp/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 
 def root(request):
     return HttpResponse("The server has started")
+
+
