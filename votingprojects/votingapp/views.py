142a3e8f30f2bde335fb308f15f926723da880fb
mehediitdba <mehediitdba@gmail.com>
2022-08-05 22:26:46 +0600

    added

--- votingprojects/votingapp/views.py
+++ votingprojects/votingapp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
+arr = ['Java','Python','Cplusplus','C','DotNET','JavaScript','PHP','Swift','SQL','Ruby', 'Delphi','Objective-C','Go','Assemblylanguage','VisualBasic','D','R','Perl','MATLAB']
+globalcnt = dict()
+
 def index(request):
-    #return HttpResponse("calculater app server started")
-    return render(request, 'index.html')
+    mydictionary={
+        "arr":arr
+    }
+    return render(request, 'index.html', context=mydictionary)
+
+def getquery(request):
+    q=request.GET['languages']
+    if q in globalcnt:
+        # if already exist then increament the value
+        globalcnt[q]=globalcnt[q]+1
+    else:
+        # first occurrence
+        globalcnt[q]=1
+    mydictionary={
+        "arr":arr,
+        "globalcnt":globalcnt
+    }
+    return render(request, 'index.html',context=mydictionary)
+    #return render(request, 'index.html', context=mydictionary)
