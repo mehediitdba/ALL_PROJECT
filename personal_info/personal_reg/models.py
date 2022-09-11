bb492ba3e7af6865eb2f5d67bc23be0a896436ca
mehediitdba <mehediitdba@gmail.com>
2022-09-12 01:01:23 +0600

    added

--- personal_info/personal_reg/models.py
+++ personal_info/personal_reg/models.py
@@ -1,4 +1,3 @@
-from turtle import title
 from django.db import models
 
 # Create your models here.
@@ -7,8 +6,8 @@ class Position(models.Model):
     title=models.CharField(max_length=50)
 
 class Personal(models.Model):
-    fullname=models.CharField(max_length=100),
-    per_code=models.CharField(max_length=15),
-    mobile=models.CharField(max_length=15),
+    fullname=models.CharField(max_length=100)
+    per_code=models.CharField(max_length=15)
+    mobile=models.CharField(max_length=15)
     position=models.ForeignKey(Position,on_delete=models.CASCADE)
 
