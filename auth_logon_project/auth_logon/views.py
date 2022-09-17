c6097bc6f50c5084ca26c9104b906b80cc5b81b5
mehediitdba <mehediitdba@gmail.com>
2022-09-18 00:02:21 +0600

    added

---
+++ auth_logon_project/auth_logon/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+# Create your views here.
+def login_user(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,("Login Success"))
+            return render(request,'authenticate/home.html', {})
+        else:
+            messages.warning(request,("There Was An Error Login In, Try Agin..."))
+            return redirect('/login/login_user')
+
+    else:
+        return render(request,'authenticate/login.html', {})
